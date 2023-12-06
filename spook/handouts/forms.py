45d11f658ea40bc7e8aadd83e46ddcb2da2230b8
Robin Graf <robingraf@hotmail.co.uk>
2023-12-06 23:42:25 +0000

    Make text input into textarea
    changes the input form to allow new lines instead of enter sending the form

--- spook/handouts/forms.py
+++ spook/handouts/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class HandoutForm(forms.Form):
     full_name = forms.CharField(max_length = 200)
-    contents_text = forms.CharField(label="Handout text", max_length=700)
+    contents_text = forms.CharField(label="Handout text", max_length=700, widget=forms.Textarea)
