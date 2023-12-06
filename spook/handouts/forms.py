28010e909bb53a18b8e1e922febb08cdd1dc1f25
Amzith <jeanamy@live.co.uk>
2023-12-06 00:03:17 +0000

    Adding a form to receive textual input

---
+++ spook/handouts/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class HandoutForm(forms.Form):
+    full_name = forms.CharField(max_length = 200)
+    contents_text = forms.CharField(label="Handout text", max_length=700)
--- spook/handouts/views.py
+++ spook/handouts/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .forms import HandoutForm
 
 # Create your views here.
 def formpage(request):
 
+    # Handle Form 
+    if request.method == "POST":
+        form = HandoutForm(request.POST)
+
+        if form.is_valid():
+            output_text = form.cleaned_data["contents_text"]
+            return render(request, 'output.html', {"output_text":output_text})
+    
+    else:
+        form = HandoutForm()
+
     context = {
         'title': 'Handouts',
-        'date': 1920
+        'date': 1920,
+        "form": form
     }
 
     return render(request, 'home.html', context=context)
+
