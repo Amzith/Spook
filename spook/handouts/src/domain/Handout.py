1ce6bc5df968212db4aa2c438dfafd57a9bf725f
Robin Graf <robingraf@hotmail.co.uk>
2023-12-08 13:41:15 +0000

    Add handout to domain
    add the handout to domain, still needs the remaining domain added to be usable

---
+++ spook/handouts/src/domain/Handout.py
@@ -0,0 +1,13 @@
+# Handout object, should contain everything needed to draw an image handout
+class Handout:
+    def __init__(self, name, background):
+        self.name = name
+        self.backgroundImage = background
+        self.texts = []
+        self.images = []
+
+    def addText(self, text):
+        self.texts.append(text)
+
+    def addImage(self, image):
+        self.images.append(image)
