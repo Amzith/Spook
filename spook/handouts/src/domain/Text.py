d5cb767a9e111242bc29f9d87536919a8d82894b
Amzith <jeanamy@live.co.uk>
2023-12-08 23:29:49 +0000

    Add text class

---
+++ spook/handouts/src/domain/Text.py
@@ -0,0 +1,8 @@
+# Text object, should be printable over an image
+class Text:
+    def __init__(self, font, position, textContent, colour, styling):
+        self.font = font
+        self.position = position
+        self.textContent = textContent
+        self.colour = colour
+        self.styling
