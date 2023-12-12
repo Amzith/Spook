baf63066014b2b226f417e87f0a4b61309821bba
Amy Hodge <jeanamy@live.co.uk>
2023-12-12 23:28:23 +0000

    Adding CSRF trusted origins to settings

--- spook/spook/settings.py
+++ spook/spook/settings.py
@@ -27,6 +27,7 @@ DEBUG =  os.environ.get('DEBUG_ON', 'True') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = [os.environ.get('CSRF_TRUSTED_ORIGINS', 'https://*.127.0.0.1')]
 
 # Application definition
 
