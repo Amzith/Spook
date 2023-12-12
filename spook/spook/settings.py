cf30ffb81844879e9d034aac8e5de4dddbf1a566
Amy Hodge <jeanamy@live.co.uk>
2023-12-12 23:19:19 +0000

    Change secret key to use environment variable
    Reassign secret key in settings.py

--- spook/spook/settings.py
+++ spook/spook/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j87*8fcb((4o*xrom2l8&zvf(^65g#5u0-68b=d5zqei$8+sk_'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-j87*8fcb((4o*xrom2l8&zvf(^65g#5u0-68b=d5zqei$8+sk_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  os.environ.get('DEBUG_ON', 'True') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
